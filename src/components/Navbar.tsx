02544060507ccdcf242efbcb44b9c40d2bf08b31
LoXur-ux <starkaw@ya.ru>
2024-05-21 15:54:33 +0300

    Created Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,49 @@
-import React, { useState } from "react";
-import { View } from "react-native";
-import NavButton from "../ui/NavButton";
+import React from "react";
+import { View, TouchableOpacity, Image, StyleSheet } from "react-native";
 import styled from "styled-components/native";
 
-const NavbarView = styled.View`
-  display: flex;
-  width: 10%;
-`;
+const icons = [
+  require("../../assets/icons/navbar/Notifications.png"),
+  require("../../assets/icons/navbar/History.png"),
+  require("../../assets/icons/navbar/QR.png"),
+  require("../../assets/icons/navbar/Scooter.png"),
+  require("../../assets/icons/navbar/Profile.png"),
+];
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
+  const handlePress = (buttonIndex: number) => {
+    console.log(`Button ${buttonIndex} pressed`);
+  };
+
   return (
-    <NavbarView>
-      <NavButton imageSource="" onPress={() => {}} selected />
-      <NavButton imageSource="" onPress={() => {}} selected />
-      <NavButton imageSource="" onPress={() => {}} selected />
-      <NavButton imageSource="" onPress={() => {}} selected />
-      <NavButton imageSource="" onPress={() => {}} selected />
-    </NavbarView>
+    <NavbarContainer>
+      {icons.map((icon, index) => (
+        <NavButton key={index} onPress={() => handlePress(index + 1)}>
+          <NavIcon source={icon} />
+        </NavButton>
+      ))}
+    </NavbarContainer>
   );
 };
+
+const NavbarContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  height: 60px;
+  background-color: #333;
+`;
+
+const NavButton = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+`;
+
+const NavIcon = styled.Image`
+  width: 50px;
+  height: 50px;
+`;
 
 export default Navbar;
